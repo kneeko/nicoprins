28f3a5e46c7eb925c9ddee69bd880a90eac59158
Nico Prins <nico@alu.local>
2015-03-18 23:01:14 -0700

    Added minor optimizations to page speed.

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,26 @@
 ;(function() {
 
+	WebFontConfig = {
+		google: {families: ['Open+Sans:400,700:latin']}
+	};
+	 
+	var cb = function() {
+		var wf = document.createElement('script');
+		wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	};
+	 
+	var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
+	 
+	if (raf) {
+		raf(cb);
+	} else {
+		window.addEventListener('load', cb);
+	}
+
 	function bind() {
 		var destination = $('.content');
 		destination.find('a[data-section]').click(function(event) {
