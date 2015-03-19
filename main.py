aefb2974565b14e07e433bc9f3ca37788e12b6ed
Nico Prins <nico@alu.local>
2015-03-19 16:09:11 -0700

    Coupled fragment and document rendering and added some styling tweaks.

--- main.py
+++ main.py
@@ -23,106 +23,69 @@ projects = [
 	"work/by-hand-and-eye",
 ]
 
-def get_fragment(section):
-	path = os.path.join(os.path.dirname(__file__), "static/fragments/%s.html" % section)
-	exists = os.path.isfile(path) 
-	if exists:
+def get_fragment(resource):
+
+	path = os.path.join(os.path.dirname(__file__), "static/fragments/%s.html" % resource)
+	if os.path.isfile(path):
 		fragment = file(path, "r").read().decode("utf8")
 		return fragment
 
+def get_document(resource, template):
+
+	values = {}
+	resource = "work" if not resource else resource
+	parts = resource.split("/")
+	section = parts[0]
+
+	title = None if resource == "work" else resource
+	try:
+		title = title if not parts[1] else parts[1]
+		title = " ".join(title.split("-"))
+	except IndexError:
+		pass
+
+	values["css"] = "static/css/main.css"
+	values["js"] = "templates/javascript.html"
+	values["title"] = title
+	values["resource"] = resource.split("/")
+	values["name"] = section
+
+	fragment = get_fragment(resource)
+	values["fragment"] = fragment
+	try:
+		index = projects.index(resource)
+		if index is not None:
+			values["project"] = True
+			pill = {}
+			pill["progress"] = str((index + 1) / float(len(projects)) * 100)
+			pill["index"] = index + 1
+			pill["length"] = len(projects)
+			prev_index = index - 1
+			if prev_index >= 0:
+				pill["prev"] = projects[prev_index]
+			next_index = index + 1
+			if next_index < len(projects):
+				pill["next"] = projects[next_index]
+			values["pill"] = pill
+	except ValueError:
+		pass
+
+	markup = template.render(values)
+	return markup
+
 class FragmentHandler(webapp2.RequestHandler):
 	def get(self, resource):
-
-		if not resource:
-			resource = "work"
-	
 		template = env.get_template("templates/fragment.html")
-		values = {}
-
-		parts = resource.split("/")
-		section = parts[0]
-
-		fragment = get_fragment(resource)
-		if fragment:
-			values["resource"] = resource.split("/")
-			values["name"] = section
-			values["fragment"] = fragment
-
-			try:
-				index = projects.index(resource)
-				if index is not None:
-					values["project"] = True
-					pill = {}
-					pill["progress"] = str((index + 1) / float(len(projects)) * 100) + "%"
-					pill["index"] = index + 1
-					pill["length"] = len(projects)
-					prev_index = index - 1
-					if prev_index >= 0:
-						pill["prev"] = projects[prev_index]
-					next_index = index + 1
-					if next_index < len(projects):
-						pill["next"] = projects[next_index]
-					values["pill"] = pill
-
-			except ValueError:
-				print("showing other")
-
-			markup = template.render(values)
-			self.response.out.write(markup)
+		markup = get_document(resource, template)
+		self.response.out.write(markup)
 
 class ResourceHandler(webapp2.RequestHandler):
 	def get(self, resource):
-
 		if resource == "work":
 			self.redirect("/")
-		elif not resource:
-			resource = "work"
-
-		title = None if resource == "work" else resource
-	
 		template = env.get_template("templates/base.html")
-		values = {}
-
-		parts = resource.split("/")
-		section = parts[0]
-
-		try:
-			title = title if not parts[1] else parts[1]
-			title = " ".join(title.split("-"))
-		except IndexError:
-			print("ya")
-
-		fragment = get_fragment(resource)
-		if not fragment:
-			self.redirect("/")
-		else:
-			values["resource"] = resource.split("/")
-			values["name"] = section
-			values["fragment"] = fragment
-			values["css"] = "static/css/main.css"
-			values["js"] = "templates/javascript.html"
-			values["title"] = title
-			try:
-				index = projects.index(resource)
-				if index is not None:
-					values["project"] = True
-					pill = {}
-					pill["progress"] = str((index + 1) / float(len(projects)) * 100) + "%"
-					pill["index"] = index + 1
-					pill["length"] = len(projects)
-					prev_index = index - 1
-					if prev_index >= 0:
-						pill["prev"] = projects[prev_index]
-					next_index = index + 1
-					if next_index < len(projects):
-						pill["next"] = projects[next_index]
-					values["pill"] = pill
-
-			except ValueError:
-				print("showing other")
-
-			markup = template.render(values)
-			self.response.out.write(markup)
+		markup = get_document(resource, template)
+		self.response.out.write(markup)
 
 application = webapp2.WSGIApplication([
 	(r'/fragment/([^\s]*)', FragmentHandler),
